55548257772f5e2e33bdea6de2a1ae420bec47e7
AaronHFT <976139616@qq.com>
2018-09-20 20:34:20 +0800

    项目初始版

---
+++ src/main/java/org/java/TnBosBackgroundApplication.java
@@ -0,0 +1,12 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnBosBackgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TnBosBackgroundApplication.class, args);
+    }
+}
